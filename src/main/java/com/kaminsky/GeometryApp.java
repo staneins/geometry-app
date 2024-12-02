a7546c36b398db07221d2ac4e02bed3558414e1b
Philip Kaminsky <kaminsky8836@gmail.com>
2024-12-02 16:05:58 +0300

    final release

---
+++ src/main/java/com/kaminsky/GeometryApp.java
@@ -0,0 +1,15 @@
+package com.kaminsky;
+
+public class GeometryApp
+{
+    public static void main( String[] args )
+    {
+        Circle circle = new Circle(24.3);
+        Triangle triangle = new Triangle(12.2, 13.2, 8.2);
+        Rectangle rectangle = new Rectangle(12.3, 15.5);
+
+        System.out.println(circle.calculateArea() + "\n" + triangle.calculateArea() + "\n" + rectangle.calculateArea());
+        System.out.println("======================================================================================");
+        System.out.println(circle.calculatePerimeter() + "\n" + triangle.calculatePerimeter() + "\n" + rectangle.calculatePerimeter());
+    }
+}
