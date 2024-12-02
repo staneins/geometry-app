a0e1ac2849e2e8eccfe255e2be6de14b8cf2a24c
Philip Kaminsky <kaminsky8836@gmail.com>
2024-12-02 16:58:07 +0300

    add module dependencies

--- src/main/java/com/kaminsky/GeometryApp.java
+++ src/main/java/com/kaminsky/GeometryApp.java
@@ -7,9 +7,13 @@ public class GeometryApp
         Circle circle = new Circle(24.3);
         Triangle triangle = new Triangle(12.2, 13.2, 8.2);
         Rectangle rectangle = new Rectangle(12.3, 15.5);
+        GeometryUtils utils = new GeometryUtils();
 
         System.out.println(circle.calculateArea() + "\n" + triangle.calculateArea() + "\n" + rectangle.calculateArea());
         System.out.println("======================================================================================");
         System.out.println(circle.calculatePerimeter() + "\n" + triangle.calculatePerimeter() + "\n" + rectangle.calculatePerimeter());
+        System.out.println("======================================================================================");
+        System.out.println(utils.compareFigures(circle, triangle));
+
     }
 }
